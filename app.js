afa4b8bd72834135e1f915e64613972e5ae7a631
GabrielAlvesFR <137641904+GabrielvFrei@users.noreply.github.com>
2024-08-28 17:55:57 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+function menuoculto() {
+    const MenuOculto = document.querySelector('.oculto');
+    const BtnOculto = document.getElementById('btnoct');
+    MenuOculto.classList.toggle('open');
+    BtnOculto.classList.toggle('ativo');
+}
